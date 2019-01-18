14119dac69073c5e9b34a1f8a8d298a7d7da8c7d
Heather Miller <heather.miller114@gmail.com>
2019-01-18 20:29:10 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -38,7 +38,8 @@ class ApplicationController < Sinatra::Base
   
   patch '/articles/:id' do
     @article = Article.find(params[:id])
-    @article.update(params)
+    @article.title = params[:title]
+    @article.content = params[:content]
     @article.save
     redirect "/articles/#{@article.id}"
   end
